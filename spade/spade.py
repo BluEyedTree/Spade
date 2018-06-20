58db95b584bcada5b8c4ab309e692da06b1cb385
Thomas Bekman <Thomas.Bekman@digitalglobe.com>
2018-06-19 21:19:15 -0600

    added intersect function + more tests.

--- spade/spade.py
+++ spade/spade.py
@@ -1,19 +1,18 @@
 # def GSP(table_of_words, chars, min_support):
 #     pass
 
-def gps_calculate_support(table_of_words, sequence):
-    """used for spade just for right now"""
-
-    support = 0
-    for word in table_of_words:
-        if sequence in word:
-            support += 1
-    return support
+# def gps_calculate_support(table_of_words, sequence):
+#     """used for spade just for right now"""
+#
+#     support = 0
+#     for word in table_of_words:
+#         if sequence in word:
+#             support += 1
+#     return support
 
 
 # def gsp_extend_prefix_tree(array_of_bottom_nodes):
 #     pass
-
 
 
 def spade(current_sequence_position_tuples, min_support, previous_sequence_position_tuples, k_or_depth):
@@ -38,18 +37,15 @@ def spade_intersect(poslist_of_sequence_to_be_extended, poslist_of_sequence_to_b
 
 # tree implementation
 class Node(object):
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, sequence, poslist):
+        self.parent = None
         self.children = []
+        self.sequence = sequence
+        self.poslist = poslist
 
     def add_child(self, obj):
         self.children.append(obj)
-
-
-def main():
-    start_tuple = (None, [])  # not finished
-    start_node = Node(start_tuple)
-    spade(start_node, 3, [], 0)
+        obj.parent = self
 
 
 """
@@ -74,22 +70,76 @@ def get_unique_supported_letters(word_database, min_support):
     return unique_supported_letters
 
 
-def init_initial_poslist(unique_letters, word_database):
-    """
-
-    :array[chars] unique_letters: all letters that have min support
-    :array[words] word_database: all the sample words
-    :return: initial poslist. For each unique min supported letter it lists the position of that letter in each sample word
-    """
-    poslist = {}
-    for char in unique_letters:
-        poslist[char] = {}
-        for word_index, word in enumerate(word_database):
-            poslist[char][word_index] = []
-
+def init_poslist(input_letter, word_database):
+    poslist_letter = {}
     for word_index, word in enumerate(word_database):
-        for char_index, letter in enumerate(word):
-            if letter in unique_letters:
-                poslist[letter][word_index].append(char_index)
-    return poslist
-#
+        poslist_letter[word_index] = []
+        for char_index, char in enumerate(word):
+            if input_letter == char:
+                poslist_letter[word_index].append(char_index)
+    return poslist_letter
+
+
+def init_spade(word_database, min_support):
+    init_node = Node(sequence=None, poslist=None)
+    unique_chars = get_unique_supported_letters(word_database, min_support)
+    init_generation = []
+    for char in unique_chars:
+        new_node = Node(sequence=char, poslist=init_poslist(char, word_database))
+        init_node.add_child(new_node)
+        init_generation.append(new_node)
+    return init_generation
+'''
+node_a is to extended
+node_b is extending node_a
+
+'''
+def intersection(node_a, node_b):
+    #The first condition is that Noda_A and Node_B are from the same sequence
+    assert (node_a.parent == node_b.parent), "Function was called with nodes of different parents"
+
+    poslist_a_b = {}
+
+    #The second condition
+    #A letter from node_to_be_extended must be before a letter in node_to_extend_with
+    for sequence in  list(node_a.poslist.keys()):
+        first_occurance_noda_a = node_a.poslist.get(sequence)[0]
+        all_occurance_node_b = node_b.poslist.get(sequence)
+        occurances_node_a_b = []
+        #Ensure that node_b has the sequence
+        if(all_occurance_node_b is not None):
+            for occurance_node_b in all_occurance_node_b:
+                if(occurance_node_b > first_occurance_noda_a):
+                    occurances_node_a_b.append(occurance_node_b)
+        #Ensure that condition two was satisfied, and occurances_node_a_b is not empty
+        if(len(occurances_node_a_b) >0 ):
+            poslist_a_b[sequence] = occurances_node_a_b
+
+    return poslist_a_b
+
+
+def extend_sequence(node_a, node_b):
+    pass
+
+
+'''
+"""
+Calculates the support of a given substring.
+The support is the number of distinct sequences that the substring occurs. Or the length of the poslist.
+Input:
+A poslist of a substring
+
+Example input:
+{1:[1,2,3]}
+
+Output:
+Integer
+
+Example output:
+1
+"""
+
+
+def calculate_support(poslist_of_substring):
+    return len(list(poslist_of_substring.keys()))
+'''
--- test_spade.py
+++ test_spade.py
@@ -4,11 +4,6 @@ except:
     from Spade.spade.spade import *
 
 
-def test_calculate_support():
-    words = ["rat", "mat"]
-    assert gps_calculate_support(table_of_words=words, sequence="at") == 2
-
-
 def test_get_unique_letters():
     words = ["abba", "ccd", "abc"]
     assert get_unique_supported_letters(word_database=words, min_support=2) == {"a", "b", "c"}
@@ -16,6 +11,59 @@ def test_get_unique_letters():
 
 def test_init_poslist():
     words = ["abba", "ccd", "abc"]
-    unique_letters = {"a", "b", "c"}
-    answer = {'c': {0: [], 1: [0, 1], 2: [2]}, 'b': {0: [1, 2], 1: [], 2: [1]}, 'a': {0: [0, 3], 1: [], 2: [0]}}
-    assert init_initial_poslist(unique_letters=unique_letters, word_database=words) == answer
+    assert init_poslist(input_letter="a", word_database=words) == {0: [0, 3], 1: [], 2: [0]}
+
+
+def test_init_spade__init_poslists():
+    words = ["abba", "ccd", "abc"]
+    current_generation = init_spade(word_database=words, min_support=2)
+    for node in current_generation:
+        if node.sequence == "a":
+            assert node.poslist == {0: [0, 3], 1: [], 2: [0]}
+        elif node.sequence == "b":
+            assert node.poslist == {0: [1, 2], 1: [], 2: [1]}
+        else:
+            assert node.poslist == {0: [], 1: [0, 1], 2: [2]}
+
+
+def test_init_spade_get_parent():
+    words = ["abba", "ccd", "abc"]
+    current_generation = init_spade(word_database=words, min_support=2)
+    assert (current_generation[0].parent == current_generation[1].parent) and (
+            current_generation[2].parent == current_generation[1].parent)
+
+
+def test_intersection():
+    # (self, sequence, poslist)
+    parent_of_a_g = Node("asdasd", {})
+
+    node_a_poslist = {0: [1, 3, 4], 1: [2, 4], 2: [1, 2]}
+    node_a = Node("a", node_a_poslist)
+    parent_of_a_g.add_child(node_a)
+
+    node_g_poslist = {0: [2, 5], 1: [1, 5], 2: [0, 3]}
+    node_g = Node("g", node_g_poslist)
+
+    parent_of_a_g.add_child(node_g)
+    output = intersection(node_a, node_g)
+
+    assert output == {0: [2, 5], 1: [5], 2: [3]}
+
+def test_extend_sequence():
+    node_a_poslist = {0: [1, 3, 4], 1: [2, 4], 2: [1, 2]}
+    node_a = Node("a", node_a_poslist)
+
+    node_g_poslist = {0: [2, 5], 1: [1, 5], 2: [0, 3]}
+    node_g = Node("g", node_g_poslist)
+
+
+    extend_sequence()
+
+
+def calculate_support():
+    poslist = {}
+    poslist[1] = [1, 2, 3]
+    poslist[2] = [1, 3]
+    poslist[3] = [2]
+    answer = 3
+    assert calculate_support(poslist_of_substring=poslist) == answer
